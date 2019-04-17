092e448ed5f8bcc0bd815629a62236daa5d36d73
Frank Zou <szou28@wisc.edu>
2019-04-16 21:34:30 -0500

    add alg

--- code/algorithm/process.m
+++ code/algorithm/process.m
@@ -2,8 +2,8 @@ function out_img = process(file_name)
     img = imread(file_name);
     img = AdvancedConstract(img);
     br = BrightnessDetection(img);
-    if br < 0.8
-        img = img + 255 * (0.8 - br);
+    if br < 0.6
+        img = img + 255 * (0.6 - br);
     end
     out_img = img;
 end
